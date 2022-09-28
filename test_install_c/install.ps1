0c054062438d6dc8f5c3432167a20c9bea7b472e
CATS <11648807+CoAuToSe@users.noreply.github.com>
2022-09-28 11:58:04 +0200

    changed tests

--- test_install_c/install.ps1
+++ test_install_c/install.ps1
@@ -2,6 +2,8 @@ Set-Location C:/
 git clone https://github.com/Microsoft/vcpkg.git
 Set-Location vcpkg
 ./bootstrap-vcpkg.bat
-setx PATH "$($Env:PATH);C:\vcpkg;"
+# setx PATH "$($Env:PATH);C:\vcpkg;"
+$current_PATH = [Environment]::GetEnvironmentVariable("PATH", "USER"); [Environment]::SetEnvironmentVariable("PATH", $current_PATH + "C:\vcpkg;", "USER")
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","MACHINE") + [Environment]::GetEnvironmentVariable("Path","USER")
 vcpkg integrate install
 vcpkg install raylib:x64-windows
--- test_install_c/main.cpp
+++ test_install_c/main.cpp
@@ -5,10 +5,45 @@
 static int screenWidth = 800;
 static int screenHeight = 450;
 
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
 int main(void) {
     std::cout << "Hello World!";
+    // Initialization (Note windowTitle is unused on Android)
+    //---------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "classic game: missile commander");
-    CloseWindow();
+
+    // InitGame();
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
+    int index = 0;
+    // Main game loop
+    while (!WindowShouldClose() && index <= 100000) // Detect window close button or ESC key
+    {
+        index++;
+        std::cout << '\r' << index;
+        
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+        // Update and Draw
+        //----------------------------------------------------------------------------------
+        // UpdateDrawFrame();
+        //----------------------------------------------------------------------------------
+    }
+#endif
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    // UnloadGame();         // Unload loaded data (textures, sounds, models...)
+
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
     std::cout << "Goodbye World!";
     return 0;
 }
