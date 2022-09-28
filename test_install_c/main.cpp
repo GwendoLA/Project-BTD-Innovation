cf3ba3bdd3aafcedcab151bb6c8a72c6a03ae723
maeva <maeva.desbats@esme.fr>
2022-09-28 13:32:05 +0200

    modif main.cpp

--- test_install_c/main.cpp
+++ test_install_c/main.cpp
@@ -2,8 +2,8 @@
 #include "raylib.h"
 #include <iostream>
 
-static int screenWidth = 800;
-static int screenHeight = 450;
+static int screenWidth = 1600;
+static int screenHeight = 900;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -14,29 +14,26 @@ int main(void) {
     //---------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "classic game: missile commander");
 
-    // InitGame();
 
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-#else
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
     int index = 0;
     // Main game loop
-    while (!WindowShouldClose() && index <= 100000) // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         index++;
         std::cout << '\r' << index;
         
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawText(TextFormat("ROUND %4i \nMONEY %4i \nVIES %4i", index, -2*index, -index), 1250, 10, 40, MAGENTA);
         EndDrawing();
         // Update and Draw
         //----------------------------------------------------------------------------------
         // UpdateDrawFrame();
         //----------------------------------------------------------------------------------
     }
-#endif
+
     // De-Initialization
     //--------------------------------------------------------------------------------------
     // UnloadGame();         // Unload loaded data (textures, sounds, models...)
