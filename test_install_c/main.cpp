8a1bc076edcc20a4f33ece38aaeb4e421d10eaca
CATS <11648807+CoAuToSe@users.noreply.github.com>
2022-09-28 09:42:42 +0200

    Update main.cpp

--- test_install_c/main.cpp
+++ test_install_c/main.cpp
@@ -1,18 +1,13 @@
 
 #include "raylib.h"
-
-#include <sqlite3.h>
 #include <iostream>
-
 
 static int screenWidth = 800;
 static int screenHeight = 450;
 
 int main(void) {
-  std::cout << sqlite3_libversion() << '\n';
-  
-  InitWindow(screenWidth, screenHeight, "classic game: missile commander");
-  CloseWindow();        // Close window and OpenGL context
-  std::cout << sqlite3_libversion() << '\n';
-  return 0;
+    std::cout << "Hello World!";
+    InitWindow(screenWidth, screenHeight, "classic game: missile commander");
+    CloseWindow();        // Close window and OpenGL context
+    return 0;
 }
