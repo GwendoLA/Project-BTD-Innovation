9fc1e3641ee7b2586d9f09eec3ae7a35c926c5af
CATS <11648807+CoAuToSe@users.noreply.github.com>
2022-09-28 12:02:48 +0200

    Update install.ps1

--- test_install_c/install.ps1
+++ test_install_c/install.ps1
@@ -4,6 +4,6 @@ Set-Location vcpkg
 ./bootstrap-vcpkg.bat
 # setx PATH "$($Env:PATH);C:\vcpkg;"
 $current_PATH = [Environment]::GetEnvironmentVariable("PATH", "USER"); [Environment]::SetEnvironmentVariable("PATH", $current_PATH + "C:\vcpkg;", "USER")
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","MACHINE") + [Environment]::GetEnvironmentVariable("Path","USER")
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","MACHINE") + [Environment]::GetEnvironmentVariable("Path","USER")
 vcpkg integrate install
 vcpkg install raylib:x64-windows
